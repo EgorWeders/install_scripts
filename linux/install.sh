e8368c911b63c54d7a01ff83a6defa9f8772290c
EgorWeders <157705588+EgorWeders@users.noreply.github.com>
2024-11-07 16:10:27 +0700

    Update install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -1,75 +1,13 @@
 #!/bin/bash
 echo ""
 
-function runCommand(){
-	commandString="$1"
-		echo "Command "${commandString}""
-	if [ -z "$commandString" ]; then
-		echo "Empty command!"
-		return 1
-	fi
-	eval "${commandString}  > /dev/null"
-	if [ $? -ne 0 ]; then
-		echo "Command Failed"
-		return 2
-	fi
-	return 0
-}
-function runImportantCommand(){
-	runCommand "$1"
-	if [ $? -ne 0 ]; then
-		echo "Stop installing"
-		exit $? 
-	fi
-	return $? 
-}
-function runOptionalCommand(){
-	runCommand "$1"
-	if [ $? -ne 0 ]; then
-		echo "But execution will go on"
-	fi
-	return $? 
-}
-
-#very basic tools such as packet manager
-function essentialsInstall(){
-	echo "Installing essentials"
-	runImportantCommand "type ip"
-	runImportantCommand "type ipd"
-}
-
-#ipconfig , proxies setting
-function netToolsInstall(){
-		echo "Installing net tools..."
-}
-
-#zsh etc.
-function cliToolsInstall(){
-		echo "Installing cli tools..."
-}
-#configure installed basic tools
-function configureBasic(){
-		echo "Configure basic tools..."
-}
-
-#entry for basic install
-function installBasic(){
-	echo "Initital basic install..."
-	essentialsInstall
-	netToolsInstall
-	cliToolsInstall
-	configureBasic
-	echo "Basic install complete"
-}
-
-
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
 
-
+source ./utils.sh
 echo "Running as root"
 echo "Initital install"
 installBasic
